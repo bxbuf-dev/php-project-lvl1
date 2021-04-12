582f07ce3cee7d5f4f46e9e5eb7165ab95e13c54
Aleksei <bxbuf-dev@outlook.com>
2021-04-12 18:45:00 +0200

    Project step 3

--- Makefile
+++ Makefile
@@ -6,3 +6,6 @@ brain-games:
 
 validate:
 	@composer validate
+
+autoload:
+	composer dump-autoload
---
+++ src/Cli.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Brain\Games\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function getUserName()
+{
+    line("Welcome to the Brain Games!");
+    $plrName = prompt("May I have your name?");
+    line("Hello %s!", $plrName);
+    
+    return $plrName;
+}
