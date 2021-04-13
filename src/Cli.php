d4426eef10ae1e7be1105f648792aaf5f3d011b3
Aleksei <bxbuf-dev@outlook.com>
2021-04-13 15:52:01 +0200

    Step 4 Guthub Actions

--- src/Cli.php
+++ src/Cli.php
@@ -9,7 +9,7 @@ function getUserName()
 {
     line("Welcome to the Brain Games!");
     $plrName = prompt("May I have your name?");
-  line("Hello %s!", $plrName);
+    line("Hello %s!", $plrName);
 
     return $plrName;
 }
