8b1db5d63c992b74a4720099f0960c656308d6c3
Aleksei <bxbuf-dev@outlook.com>
2021-04-15 18:12:47 +0200

    Step 7. Brain GCD game comleted

---
+++ src/Games/Gcd.php
@@ -0,0 +1,34 @@
+<?php
+
+//Логика игры НОД
+//Формулировка условия
+//Критерии правильно/нерпавильно
+
+namespace Brain\Games\Gcd;
+
+function gameGcd(int $maxQuestions)
+{
+    $rules = 'Find the greatest common divisor of given numbers.';
+
+    $questions = [];
+    $answers = [];
+
+    for ($i = 0; $i < $maxQuestions; $i++) {
+        $num1 = rand(0, 99);
+        $num2 = rand(0, 99);
+        $questions[$i] = "{$num1} : {$num2}";
+        $answers[$i] = strval(gcd($num1, $num2));
+    }
+    $gameConditions = [$rules, $questions, $answers];
+    return $gameConditions;
+}
+
+function gcd($num1, $num2)
+{
+    while ($num2 != 0) {
+        $tmp = $num1 % $num2;
+        $num1 = $num2;
+        $num2 = $tmp;
+    }
+    return $num1;
+}
