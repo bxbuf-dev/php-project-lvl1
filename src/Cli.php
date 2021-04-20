5b081f6804fe66f50df53e9ca5b90f7eb19cb253
Aleksei <bxbuf-dev@outlook.com>
2021-04-20 17:38:32 +0200

    Cli.php errors

--- src/Cli.php
+++ src/Cli.php
@@ -5,16 +5,16 @@ namespace Brain\Games\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function getUserName()
+function getUserName(): string
 {
     line("Welcome to the Brain Games!");
-    $plrName = prompt("May I have your name?", false, ' ');
+    $plrName = prompt("May I have your name?");
     line("Hello, %s!", $plrName);
 
     return $plrName;
 }
 
-function replyToPlayer($name, $result)
+function replyToPlayer(string $name, bool $result): void
 {
     if ($result) {
         line("Congratulations, {$name}!");
@@ -24,7 +24,7 @@ function replyToPlayer($name, $result)
     return;
 }
 
-function menuGame()
+function menuGame(): int
 {
     line("We have games:");
     line("1. Even / Odd");
