962349b483debdae49be83038db1c19418796c51
Aleksei <bxbuf-dev@outlook.com>
2021-04-26 14:50:29 +0200

    Prime logic fixed

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -25,6 +25,9 @@ function gamePrime(): void
 
 function isPrime(int $num): bool
 {
+    if ($num <= 1) {
+        return false;
+    }
     for ($i = 2; $i < $num; $i++) {
         if ($num % $i == 0) {
             return false;
