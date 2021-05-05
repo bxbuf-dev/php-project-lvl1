37343902cd1d62f77051f247c9df340a78162770
Aleksei <bxbuf-dev@outlook.com>
2021-05-05 18:26:49 +0200

    minor fix

--- src/Cli.php
+++ src/Cli.php
@@ -5,11 +5,10 @@ namespace Brain\Games\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function greetings(): string
+function greeting()
 {
     line("Welcome to the Brain Games!");
     $plrName = prompt("May I have your name?");
     line("Hello, %s!", $plrName);
-
-    return $plrName;
+    return;
 }
